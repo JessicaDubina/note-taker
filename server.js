76a58772c6104a542544fd2a41ea872c5c191d90
jdub <jessicaclaing@gmail.com>
2024-01-28 20:59:37 -0800

    added handler for notes get request

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -8,7 +8,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+//landing page handler
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
+
+//handler for get notes request
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
 
 
